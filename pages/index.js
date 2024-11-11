dfdb2475b6b4537c1091c4656758dc60fc645e5d
Damien Rincon <70782966+SleepyDamien@users.noreply.github.com>
2024-11-11 11:15:40 -0600

    Update index.js
    added github link to the home page

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,32 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Link from 'next/link'
 import shopAppMain from './shopapp'
+import { FaSquareGithub } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Damien Showcase | Next.JS sandbox</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="App in progress!" />
+        <Header title="Next.JS demos" />
         <p className="description">
-         Keep an eye on this space. 
+        Each app is designed with a general concept in mind. 
         </p>
-              <div>
+    <p>
+    <Link href="https://github.com/SleepyDamien"><FaSquareGithub />View my GitHub!</Link> 
+
+    </p>
+
+              <div className="miniNav">
                   Custom mini applications
                   <ul>
                       <li><Link href="shopapp">Shop App Demo</Link> </li>
                   </ul>
               </div>
       </main>
-
       <Footer />
     </div>
   )
