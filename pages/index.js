bcf4870da27d24721a091400e229ea267f48058b
Damien Rincon <70782966+SleepyDamien@users.noreply.github.com>
2024-11-21 03:56:35 -0600

    Update index.js
    Update home page via GitHub mobile

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,18 @@ export default function Home() {
       <main id="landingPage">
         <Header title="Next.JS demos" />
         <p className="description">
-        Each app is designed with a general concept in mind. 
+        Each app is designed with a general concept in mind. Keep in mind these are conceptual and WIP applications I do in my free time. 
+    Feel free to reach out to me via linkedin!
         </p>
     <p>
     <Link href="https://github.com/SleepyDamien"><FaSquareGithub /> View my GitHub!</Link> 
     </p>
 
               <div className="miniNav">
-                  Custom mini applications
+                  Custom mini applications 
                   <ul>
                       <li><Link href="shopapp"><FaLink /> Shop App Demo</Link> </li>
+                      <li><Link href="RPGidler"><FaLink /> Idle RPG game Demo</Link> </li>
                   </ul>
               </div>
       </main>
