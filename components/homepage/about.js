7ddc5e91e6fff5711cf53f7aa90298804a1f9be1
Damien Rincon <70782966+SleepyDamien@users.noreply.github.com>
2025-05-21 12:13:27 -0500

    Update about.js

--- components/homepage/about.js
+++ components/homepage/about.js
@@ -7,6 +7,8 @@ export default function About() {
             <span>
                 <p>I am a full stack dev with 9 years of experience, who is constantly looking to expand their skillset!</p>
                 <p>Feel free to check out the micro-sites below, or view the code directly on GitHub!</p>
+                <p>Check out my blog if you want some of my knowledge share.</p>
+                <Link href="https://blog.1024kb.dev">blog.1024kb.dev</Link>
             </span>
         </div>
     );
