61039806627bf7c13dbbe76021dba18453225459
Damien Rincon <70782966+SleepyDamien@users.noreply.github.com>
2025-05-21 12:09:10 -0500

    Update socials.js
    add blog link

--- components/homepage/socials.js
+++ components/homepage/socials.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import styles from '@styles/HomePage.module.css'
-import { FaSquareGithub, FaLinkedin, FaSquareInstagram, FaYoutube } from "react-icons/fa6";
+import { FaSquareGithub, FaLinkedin, FaSquareInstagram, FaYoutube, FaWordpress } from "react-icons/fa6";
 
 export default function SocialLinks() {
     return (
         <div className={styles.socialLinksContainer}>
+            <Link href="https://blog.1024kb.dev"><FaWordpress /></Link> 
             <Link href="https://github.com/SleepyDamien"><FaSquareGithub /></Link> 
             <Link href="https://linkedin.com/in/damien-rincon"><FaLinkedin /></Link> 
             <Link href="https://instagram.com/1024kb.dev"><FaSquareInstagram /></Link> 
